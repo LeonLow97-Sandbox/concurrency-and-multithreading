029f3e86c98c17c4d644305bf156161371a15419
LeonLow97 <lowjiewei@gmail.com>
2023-06-09 18:09:09 +0800

    Sequential Processing

---
+++ holczer_balazs/_001_threads_manipulation/App.java
@@ -0,0 +1,33 @@
+package holczer_balazs._001_threads_manipulation;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        // Sequential Processing
+        Runner1 runner1 = new Runner1();
+        Runner2 runner2 = new Runner2();
+
+        runner1.execute();
+        runner2.execute();
+    }
+
+}
+
+
+class Runner1 {
+    public void execute() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Runner1: " + i);
+        }
+    }
+}
+
+
+class Runner2 {
+    public void execute() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Runner2: " + i);
+        }
+    }
+}
