c42ac91bb29f46afa0fc7429009f0b68b6d7aed8
LeonLow97 <lowjiewei@gmail.com>
2023-06-09 18:14:45 +0800

    Implementing the Runner interface

--- holczer_balazs/_001_threads_manipulation/App.java
+++ holczer_balazs/_001_threads_manipulation/App.java
@@ -4,30 +4,37 @@ public class App {
 
     public static void main(String[] args) {
 
-        // Sequential Processing
-        Runner1 runner1 = new Runner1();
-        Runner2 runner2 = new Runner2();
+        // Multithreading - time-slicing algorithm (using the same single processor)
+        // NOT parallel execution !!!!!
+        Thread t1 = new Thread(new Runner1());
+        Thread t2 = new Thread(new Runner2());
 
-        runner1.execute();
-        runner2.execute();
+        t1.start();
+        t2.start();
     }
 
 }
 
 
-class Runner1 {
-    public void execute() {
+// Runnable Interface
+class Runner1 implements Runnable {
+
+    @Override
+    public void run() {
         for (int i = 0; i < 10; i++) {
             System.out.println("Runner1: " + i);
         }
     }
+
 }
 
+class Runner2 implements Runnable {
 
-class Runner2 {
-    public void execute() {
+    @Override
+    public void run() {
         for (int i = 0; i < 10; i++) {
             System.out.println("Runner2: " + i);
         }
     }
+
 }
