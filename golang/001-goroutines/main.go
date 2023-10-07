3673623e74d0a172004db3c30ed44f8959bb00cb
LeonLow97 <lowjiewei@gmail.com>
2023-10-07 21:52:20 +0800

    Introduction to goroutines

---
+++ golang/001-goroutines/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func printSomething(s string) {
+	fmt.Println(s)
+}
+
+func main() {
+	
+	go printSomething("This is the first thing to be printed!")
+
+	printSomething("This is the second thing to be printed!")
+}
