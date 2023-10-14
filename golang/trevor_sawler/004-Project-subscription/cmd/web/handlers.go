c181212aa82f4333406ba5897d8ab136da0e7cdf
LeonLow97 <lowjiewei@gmail.com>
2023-10-14 22:23:55 +0800

    Testing handlers.go

--- golang/trevor_sawler/004-Project-subscription/cmd/web/handlers.go
+++ golang/trevor_sawler/004-Project-subscription/cmd/web/handlers.go
@@ -43,7 +43,7 @@ func (app *Config) PostLoginPage(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// check password
-	validPassword, err := user.PasswordMatches(password)
+	validPassword, err := app.Models.User.PasswordMatches(password)
 	if err != nil {
 		app.Session.Put(r.Context(), "error", "Invalid credentials")
 		http.Redirect(w, r, "/login", http.StatusSeeOther)
@@ -148,7 +148,7 @@ func (app *Config) ActivateAccount(w http.ResponseWriter, r *http.Request) {
 	}
 
 	u.Active = 1
-	err = u.Update()
+	err = app.Models.User.Update(*u)
 	if err != nil {
 		app.Session.Put(r.Context(), "error", "Unable to update user.")
 		http.Redirect(w, r, "/", http.StatusSeeOther)
